1ec0f04ec2759fc4a10e17128842af1797ae634e
Sandy Obare <obaresandy@gmail.com>
2024-12-07 02:48:54 +0300

    server.js initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
