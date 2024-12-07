534032f247bce04af5b3a19cab6ebd39ead35ddd
Sandy Obare <obaresandy@gmail.com>
2024-12-07 14:31:24 +0300

    index.js initial commit: status and stats routes implementation

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const AppController = require('../controllers/AppController');
+// const AuthController = require('../controllers/AuthController');
+// const UsersController = require('../controllers/UsersController');
+// const FilesController = require('../controllers/FilesController');
+
+const router = express.Router();
+
+router.get('/status', AppController.status);
+
+router.get('/stats', AppController.stats);
+
+module.exports = router;
