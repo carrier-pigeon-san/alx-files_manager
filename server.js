f16e85316cf8827fa221ba4093990ec67289621e
Sandy Obare <obaresandy@gmail.com>
2024-12-07 19:38:11 +0300

    server.js update: initializing express app with middleware for parsing json

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const router = require('./routes/index');
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+
 app.use('/', router);
 
 app.listen(port, () => {
