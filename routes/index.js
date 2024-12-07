207ae70ccb601fe2ee62ddaac80dfa5db473cddc
Sandy Obare <obaresandy@gmail.com>
2024-12-07 19:39:11 +0300

    routes/index.js update: addition of users route to router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 // const AuthController = require('../controllers/AuthController');
-// const UsersController = require('../controllers/UsersController');
+const UsersController = require('../controllers/UsersController');
 // const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get('/status', AppController.status);
 
 router.get('/stats', AppController.stats);
+
+router.post('/users', UsersController.users);
 
 module.exports = router;
