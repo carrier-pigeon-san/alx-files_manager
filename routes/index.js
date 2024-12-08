e6085e355614ce3a81c8dd96f5ff678ca0741c53
Sandy Obare <obaresandy@gmail.com>
2024-12-08 13:21:50 +0300

    routes/index.js update: addition of connect, disconnect, and users/me routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
-// const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
 // const FilesController = require('../controllers/FilesController');
 
@@ -11,5 +11,11 @@ router.get('/status', AppController.status);
 router.get('/stats', AppController.stats);
 
 router.post('/users', UsersController.users);
+
+router.get('/connect', AuthController.connect);
+
+router.get('/disconnect', AuthController.disconnect);
+
+router.get('/users/me', UsersController.userMe);
 
 module.exports = router;
